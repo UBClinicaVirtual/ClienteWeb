c417eb9bf435251bc18f9ecd97ff1dcf7605f595
Espina <ge250053@ncr.com>
2019-03-25 21:55:20 -0300

    Se crea componente turnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AppointmentsComponent
   ],
   imports: [
     BrowserModule,
