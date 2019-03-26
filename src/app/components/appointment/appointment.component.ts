a45eb564dd3cd9842c18eb736eae27c0549e9cef
Espina <ge250053@ncr.com>
2019-03-26 19:29:59 -0300

    Se muestran los turnos

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-appointment',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointment.component.css']
 })
 export class AppointmentComponent implements OnInit {
+  @Input() turno: {};
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit() {
   }
+
 
 }
