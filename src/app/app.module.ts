68133d3f9bf31fc1bc7d478aee3f92b39bb860c7
Espina <ge250053@ncr.com>
2019-03-25 21:57:35 -0300

    Se mueve appointments a components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { AppointmentsComponent } from './appointments/appointments.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
 
 @NgModule({
   declarations: [
