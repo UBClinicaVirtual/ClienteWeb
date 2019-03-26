688a89774d960abac6b506fc3ebb9c300ade7c13
Espina <ge250053@ncr.com>
2019-03-26 19:31:58 -0300

    Se muestra detalle del turno

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-appointment',
@@ -8,10 +9,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class AppointmentComponent implements OnInit {
   @Input() turno: {};
 
-  constructor() {  }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit() {
   }
-
+  open(content) {
+    this.modalService.open(content);
+  }
 
 }
