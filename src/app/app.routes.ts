e1f33aacfa637988b06482509f9027f415dd7277
Fernando I Romero <fer.romero1991@gmail.com>
2019-03-27 09:07:19 -0300

    se crea el app.routes , se crean los path para el navbar

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+
+import {RouterModule, Routes} from '@angular/router';
+
+import {HomeComponent} from './components/home/home.component';
+import {AppointmentsComponent} from './components/appointments/appointments.component';
+import {ProtectedComponent} from './components/protected/protected.component';
+
+
+
+//import {AuthGuardService} from "./services/auth-guard.service";
+
+const APP_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'turns', component: AppointmentsComponent },
+  { path: 'protected', component: ProtectedComponent /*, canActivate:[AuthGuardService]*/},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
