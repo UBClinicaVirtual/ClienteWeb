1c744df6f1b5fff4dd0ac43f5176003513bd5d47
Fernando I Romero <fer.romero1991@gmail.com>
2019-03-27 17:59:43 -0300

    se agrega las conciciones al path routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,14 @@ import {HomeComponent} from './components/home/home.component';
 import {WelcomeComponent} from './components/welcome/welcome.component';
 import {ProtectedComponent} from './components/protected/protected.component';
 
-
+import  {AuthGuardService} from "./services/auth-guard.service";
 
 //import {AuthGuardService} from "./services/auth-guard.service";
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'turns', component: WelcomeComponent },
-  { path: 'protected', component: ProtectedComponent /*, canActivate:[AuthGuardService]*/},
+  { path: 'turns', component: WelcomeComponent  , canActivate:[AuthGuardService]},
+  { path: 'protected', component: ProtectedComponent , canActivate:[AuthGuardService]},
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
