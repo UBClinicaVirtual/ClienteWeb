ffc60bc59f2b8b2ee8ac67906856a2edda941374
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-04-05 19:03:18 -0300

    pruebas al sv real

--- src/app/services/server-connection/server-connection.service.ts
+++ src/app/services/server-connection/server-connection.service.ts
@@ -8,7 +8,7 @@ export class ServerConnectionService {
 
   constructor(private http : HttpClient) { }
 
-  private urlPath = "https://ubclinicavirtual.000webhostapp.com/api/v1";
+  private urlPath = "http://ubclinicavirtual.000webhostapp.com/api/v1";
 
   private meth = {post: "POST", get:"GET"};
 
@@ -17,10 +17,8 @@ export class ServerConnectionService {
   public token = "";
 
   login(){
-    return this.http.post(this.urlPath, {
-      "access_token": this.token
-    }, {
-      headers: { "Accept": "application/json", "Content-Type": "application/json" }
+    return this.http.get(this.urlPath + "/user", {
+      headers: { "Accept": "application/json", "Content-Type": "application/json", "Authorization": "Bearer qsi0jBa0fyhgvZKY0mvcQTwvH9jAZ8bBAk1uVMn1OtEW6I8ypSenF3g3iQXu" }
     }).toPromise();
   }
 
