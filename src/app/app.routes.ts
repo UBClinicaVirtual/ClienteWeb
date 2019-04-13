1b610fddcd7360fb731620388917459bb9035332
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-04-12 21:14:20 -0300

    se vuelve a poner el navbar comentado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import {WelcomeComponent} from './components/welcome/welcome.component';
 import {ProtectedComponent} from './components/protected/protected.component';
 
 import  {AuthGuardService} from "./services/auth-guard.service";
+import { AppointmentsComponent } from './components/appointments/appointments.component';
 
 //import {AuthGuardService} from "./services/auth-guard.service";
 
 const APP_ROUTES: Routes = [
-  { path: 'turns', component: WelcomeComponent  , canActivate:[AuthGuardService]},
+  { path: 'home', component:WelcomeComponent  , canActivate:[AuthGuardService]},
+  { path: 'turns', component:AppointmentsComponent  , canActivate:[AuthGuardService]},
   { path: 'protected', component: ProtectedComponent , canActivate:[AuthGuardService]},
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
