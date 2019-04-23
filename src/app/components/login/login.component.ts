d9139ee35a9bb389f8235e364051c1b27d74338d
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-04-23 19:51:33 -0300

    se cambia el client id para lograr mas encapsulamiento sobre la config

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,24 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import {GoogleSignInSuccess} from 'angular-google-signin';
 import { ServerConnectionService } from 'src/app/services/server-connection/server-connection.service';
 import { LoginService } from 'src/app/services/server-connection/requests/login/login.service';
+import { ServerConfigService } from 'src/app/config/server-config.service';
+import { config } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, componentResponseInterface {
 
-  constructor(private connection: ServerConnectionService,private loginService: LoginService,) { }
+  response(data: any) {
+    this.connected = this.connection.connected;
+  }
 
-  private myClientId: string = '154525363401-7rn9a462ng2ukds3pcgff8jp4uv9c9tu.apps.googleusercontent.com';
+  constructor(private connection: ServerConnectionService,private loginService: LoginService,private config:ServerConfigService) { }
+
+  private myClientId: string = this.config.getClientID();
+
+  connected = false;
 
   onGoogleSignInSuccess(event: GoogleSignInSuccess) {
     let googleUser: any = event.googleUser;
     let id: string = googleUser.getId();
     let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
     this.connection.token = googleUser.Zi.id_token;
-    this.loginService.execute();
+    this.loginService.execute(this);
     console.log(this.connection.token);
 
   }
