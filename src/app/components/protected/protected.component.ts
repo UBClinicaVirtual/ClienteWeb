5cb7b87f2ed7163a4727a4a711c573a426b15481
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-04-24 16:23:12 -0300

    reinvencion de perfil

--- src/app/components/protected/protected.component.ts
+++ src/app/components/protected/protected.component.ts
@@ -27,4 +27,81 @@ export class ProtectedComponent implements OnInit {
     //   }
   }
 
+  usertype(type){
+    switch (type) {
+      case '0':
+      return "Sin Perfil";
+        
+      break;
+      case '1':
+      return "Paciente";
+      
+      break;
+      case '2':
+      return "Doctor";
+      
+      break;
+      case '3':
+      return "Clinica";
+        
+      break;
+      default:
+      return "No asignado";
+        break;
+    }
+  }
+
+  noteditable='readonly';
+
+  user = {
+    "user": {
+        "id": 6,
+        "email": "ubelarga@gmail.com",
+        "created_at": "2018-09-20 17:30:52",
+        "updated_at": "2018-09-27 15:06:20",
+        "api_token": "HoFcCOSgtjyfD5GELBuyQ3xHZasPiBSyqGsFeDsYLXj4BewYcIEOvyqAn0iZ",
+        "active": 1,
+        "user_type_id": "1"
+    },
+	"patient":{
+		"id": 6,
+		"first_name": "walter",
+		"last_name": "ub",        
+		"created_at": "2018-09-20 17:30:52",
+		"updated_at": "2018-09-27 15:06:20",
+		"identification_number": "0303456",
+		"birth_date": "1987-01-01",
+		"gender_id": 0,
+		"address": "Street 1234",
+		"phone": "1234-4758"
+	},
+	"hcp":{
+		"id": 6,
+		"first_name": "MD. walter",
+		"last_name": "ub",        
+		"created_at": "2018-09-20 17:30:52",
+		"updated_at": "2018-09-27 15:06:20",
+		"identification_number": "0303456",
+		"register_number": "RG3685",
+		"birth_date": "1987-01-01",
+		"gender_id": 0,
+		"address": "Street 1234",
+		"phone": "1234-4758",
+		"specialities": [ {"id": 1, "name": "Guardia"} ]
+	},
+	"clinic":{
+		"id": 6,
+		"created_at": "2018-09-20 17:30:52",
+		"updated_at": "2018-09-27 15:06:20",		
+		"business_name": "Clinica San Martin",
+		"business_number": "2003034567",		
+		"address": "Street 1234",
+		"phone": "1234-4758",
+		"hcp_specialities": [ {"id": 44, "hcp_id": 33, "speciality_id": 12}],
+		"hcps": [ {"id": 33, "first_name": "Dr. Juan", "last_name": "Perez"} ],
+		"specialities": [ {"id": 12, "name": "Guardia"} ]
+	}
+	
+}
+
 }
