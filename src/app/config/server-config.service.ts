9c71a53a339e343a2bf6f8a4b77ac8e5b7f043fb
PedroRodriguez95 <Pedro.rodriguez6@comunidad.ub.edu.at>
2019-04-26 20:06:22 -0300

    Se cambian variables del config para poder apuntar al mock server

--- src/app/config/server-config.service.ts
+++ src/app/config/server-config.service.ts
@@ -7,12 +7,13 @@ export class ServerConfigService {
 
   constructor() { }
 
-  private mockUrl = "localhost:3200";
+  private mockUrl = "http://localhost:3000";
   private realUrl = "http://ubclinicavirtual.000webhostapp.com/api/v1";
   private routerUrl= "http://localhost:3000/utils/router/router.php?url=http://ubclinicavirtual.000webhostapp.com/api/v1";
   private clientId = '154525363401-7rn9a462ng2ukds3pcgff8jp4uv9c9tu.apps.googleusercontent.com';
   url(){
-    return this.routerUrl;
+    //return this.routerUrl;
+   return this.mockUrl;
   }
 
   getClientID(){
