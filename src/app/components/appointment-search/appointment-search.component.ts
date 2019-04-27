9b49bca4bfb5f87aa6f5ff3b467372d3a68c150a
PedroRodriguez95 <Pedro.rodriguez6@comunidad.ub.edu.at>
2019-04-26 22:28:16 -0300

    Se genera metodos y componentes para la busqueda de turnos disponibles

--- src/app/components/appointment-search/appointment-search.component.ts
+++ src/app/components/appointment-search/appointment-search.component.ts
@@ -15,6 +15,7 @@ import { GetAvailableAppointmentsService } from 'src/app/services/server-connect
 export class AppointmentSearchComponent implements OnInit, componentResponseInterface {
  
   isLoading:boolean = false;
+  mostrarTurnos:boolean = false;
   requestType: string = "";
  
   constructor(
@@ -26,7 +27,6 @@ export class AppointmentSearchComponent implements OnInit, componentResponseInte
     ){
     this.getOptionData();
   }
-
 
   public clinics: optionData[] = [];
   public specialities: optionData[] = [];
@@ -50,6 +50,7 @@ export class AppointmentSearchComponent implements OnInit, componentResponseInte
     console.log(this.filtros);
     this.requestType = "getAvailableAppointments";
     this.getAvailableAppointments.execute(this);
+
   }
 
 
@@ -77,6 +78,10 @@ export class AppointmentSearchComponent implements OnInit, componentResponseInte
     return this.filtros;
   }
 
+  public imprimir(){
+    console.log(this.turnos)
+  }
+
 
   private parseClincis(clinics){
     for ( var clinic in clinics){
@@ -96,7 +101,7 @@ export class AppointmentSearchComponent implements OnInit, componentResponseInte
     }
   }
 
-  turnos;
+  public turnos;
 
   requestTypeOperation = {
     "getClinics":function(component,data){
@@ -118,7 +123,9 @@ export class AppointmentSearchComponent implements OnInit, componentResponseInte
     },
     "getAvailableAppointments": function(component,data){
         console.log(data);
-        this.turnos = data['appointments_available'];
+        component.turnos = data['available_appointments'];
+        component.mostrarTurnos = true;
+      
     }
   }
 }
