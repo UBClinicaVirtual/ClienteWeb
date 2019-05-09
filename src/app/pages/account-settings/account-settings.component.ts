161aad15a770b1f8d47814f8435303834ffbf49c
Fernando Romero <fer.romero1991@gmail.com>
2019-05-09 12:08:56 -0300

    se corrigue para posicionar el check segun el tema elegido

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -14,12 +14,23 @@ export class AccountSettingsComponent implements OnInit {
   ngOnInit() {
   }
 
-  cambiarColor(tema: string){
+  cambiarColor(tema: string, link: any){
 
-    console.log(tema);
+    this.aplicarCheck(link);
     let url = `assets/css/colors/${tema}.css`;
     this._document.getElementById('tema').setAttribute('href' , url );
 
   }
+
+  aplicarCheck(link : any ){
+    let selectores:any = document.getElementsByClassName('selector');
+    for(let ref of selectores){
+        ref.classList.remove('working');
+    }
+
+    link.classList.add('working');
+
+  } 
+ 
 
 }
