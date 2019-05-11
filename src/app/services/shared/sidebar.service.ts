3f0573a329d58a8826da833fcd95284813606254
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-05-11 01:34:26 -0300

    se modifica el servicio de sidebar, asi cargo en el menu los items correspondientes (segun perfil por ejemplo)

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -5,5 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class SidebarService {
 
+  menu:any = [
+    {
+      titulo: 'Principal',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        {titulo: 'Dashboard', url: '/dashboard'},
+        {titulo: 'Barra de progreso (prueba) ', url: '/progress'},
+        {titulo: 'Graficas(prueba)', url: '/graficas1'},
+        {titulo: 'Mis turnos', url: '/turnos'},
+        {titulo: 'Mis turnos', url: '/search-turn'}
+      ]
+    }
+  ];
+
   constructor() { }
 }
