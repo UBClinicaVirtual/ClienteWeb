ab5b20af1d8e34deefe588699853f73d0ee7283b
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-05-12 22:02:23 -0300

    se aplica nuevo metodo para el log in con google
    se obtiene el mismo token que se obtenia con el login1.
     mas info : https://developers.google.com/identity/sign-in/web/listeners

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login2',
@@ -10,10 +11,42 @@ declare function init_plugins();
 })
 export class Login2Component implements OnInit {
 
+  auth2: any;
+
   constructor(public router: Router) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+
+
+  }
+
+  googleInit(){
+
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '154525363401-7rn9a462ng2ukds3pcgff8jp4uv9c9tu.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // INFORMACION DE LA CUENTA
+        scope: 'profile email'
+
+      });
+
+      this.attachSignin(document.getElementById('btnGoogle'))
+    });;
+  }
+
+  attachSignin(element){
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+
+      //let profile = googleUser.getBasicProfile();
+
+      let token = googleUser.getAuthResponse().id_token;
+
+      console.log(token);
+
+    });
   }
 
   ingresar(){
