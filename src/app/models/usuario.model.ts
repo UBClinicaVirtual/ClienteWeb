2e73536ac06aa8c9c5f7016ee9c868ac93c46b92
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-05-14 22:58:49 -0300

    creo un usuario.model

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,17 @@
+export class Usuario{
+
+
+    constructor(
+
+        // provisorio
+        public nombre: string,
+        public apellido:string,
+        public dni: string,
+        public nacimiento: string,
+        public sexo: string,
+        public direccion: string,
+        public telefono: string
+
+    ){}
+
+}
