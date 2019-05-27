4d9c0aa571243988990684a218cd2dcdf3453a76
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-05-27 20:39:11 -0300

    Creo un select para traer las especialidades necesarias

--- src/app/register/usertype-register/usertype-register.component.ts
+++ src/app/register/usertype-register/usertype-register.component.ts
@@ -8,19 +8,30 @@ import { GetSpecialitiesService } from 'src/app/services/server-connection/reque
 })
 export class UsertypeRegisterComponent implements OnInit, componentResponseInterface {
   response(data: any) {
-    throw new Error("Method not implemented.");
+    console.log(data);
+    this.specialities = data.specialities;
   }
 
   @Input() user: any;
 
-  constructor(private specialities : GetSpecialitiesService) { }
+  specialities;
+  specialities_chosen = [];
+
+  constructor(private getSpecialitiesService : GetSpecialitiesService) { }
 
   ngOnInit() {
     if(this.user){
       console.log(this.user);
     }
 
-    this.specialities.execute(this);
+    this.getSpecialitiesService.execute(this);
+  }
+
+  specialitieChoosed(id){
+    let specChoosed = document.getElementById(id);
+    specChoosed.hidden = "hidden";
+    this.specialities_chosen.push(specChoosed.value);
+    console.log(this.specialities_chosen);
   }
 
 }
