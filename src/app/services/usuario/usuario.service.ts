e7939f8214fde7ba4d98289fb4fd1799373d4bfc
Fernando Romero <fer.romero1991@gmail.com>
2019-06-05 13:30:36 -0300

    importo usuario,usuariogoogle y patient. creo metodo guardargoogle y guardarpaciente. asi tambien agregandolos donde corresponde para la carga y limpiar

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Patient } from 'src/app/models/patient.model';
+import { UsuarioGoogle } from 'src/app/models/usuarioGoogle.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,9 @@ export class UsuarioService {
 
   token: string;
   estado: string;
+  usuario: Usuario;
+  usuarioGoogle: UsuarioGoogle;
+  paciente: Patient;
 
   constructor(
     public http: HttpClient,
@@ -27,10 +32,17 @@ export class UsuarioService {
      if(localStorage.getItem('token') && localStorage.getItem('estado')){
        this.token = localStorage.getItem('token');
        this.estado = localStorage.getItem('estado');
+       this.usuario = JSON.parse(localStorage.getItem('usuario'));
+       this.usuarioGoogle = JSON.parse(localStorage.getItem('usuarioGoogle'));
+       this.paciente = JSON.parse(localStorage.getItem('paciente'));
+
        
      }else{
        this.token = '';
        this.estado = '';
+       this.usuario = null;
+       this.usuarioGoogle= null;
+       this.paciente=null;
      }
    }
 
@@ -39,9 +51,29 @@ export class UsuarioService {
     localStorage.setItem('token',token);
     localStorage.setItem('estado', estado);
 
+  
     this.token = token;
     this.estado = estado;
+ 
 
+   }
+
+   guardarGoogle( email:string, name:string, image:string , idToken:string){
+
+    
+     this.usuarioGoogle = new UsuarioGoogle(name,image,email,idToken);
+
+     localStorage.setItem('usuarioGoogle', JSON.stringify(this.usuarioGoogle));
+     
+
+   }
+
+   guardarPaciente(paciente:Patient){
+
+    this.paciente = paciente;
+
+    localStorage.setItem('paciente', JSON.stringify(this.paciente));
+    
 
    }
 
@@ -49,8 +81,12 @@ export class UsuarioService {
     
     this.token='';
     this.estado='';
+    this.usuario = null;
     localStorage.removeItem('token');
     localStorage.removeItem('estado');
+    localStorage.removeItem('usuario');
+    localStorage.removeItem('usuarioGoogle');
+    localStorage.removeItem('paciente');
 
   
     this.router.navigate(['./login2']);
