9f1aeb076d5e7cc85756eb1f3a9935f4252e9690
Fernando Romero <fer.romero1991@gmail.com>
2019-06-05 13:31:05 -0300

    creo que el modelo de usuariogoogle

---
+++ src/app/models/usuarioGoogle.model.ts
@@ -0,0 +1,15 @@
+export class UsuarioGoogle{
+
+
+    constructor(
+
+        // provisorio
+        
+        public fullName:string,
+        public image: string,
+        public email: string,
+        public idToken?: string
+
+    ){}
+
+}
