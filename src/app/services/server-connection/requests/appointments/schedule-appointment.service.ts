591c5a7581d0bd7db340c54d241fa2f1dc309d21
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-06-06 20:45:14 -0300

    Se configura el servicio para pedir un turno

--- src/app/services/server-connection/requests/appointments/schedule-appointment.service.ts
+++ src/app/services/server-connection/requests/appointments/schedule-appointment.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { ServerConnectionService } from '../../server-connection.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ScheduleAppointmentService {
+export class ScheduleAppointmentService implements serviceNotifyInterface{
+  
+  constructor(private connection: ServerConnectionService) { }
 
-  constructor() { }
+  notifty(data: any) {
+    this.componente.response(data);
+  }
+
+  componente: componentResponseInterface;
+
+  execute(component:componentResponseInterface, body:any){
+
+    this.componente = component;
+
+    console.log("service: schedule-appointment");
+
+    let httpHeaderss = new HttpHeaders({
+      'Authorization' : 'Bearer ' + this.connection.apiKey
+    });
+
+    this.connection.post('/user/patient/appointment/schedule',httpHeaderss,body,this);
+  }
 }
