5992984089d4d796eda3becec7689d883a248a64
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-06-07 02:01:09 -0300

    Se puede sacar un turno

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ScheduleAppointmentService } from 'src/app/services/server-connection/requests/appointments/schedule-appointment.service';
 
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
   styleUrls: ['./appointment.component.css']
 })
-export class AppointmentComponent implements OnInit {
-  @Input() turno: {};
+export class AppointmentComponent implements OnInit, componentResponseInterface {
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+  response(data: any) {
+    alert("Turno solicitado correctamente");
+  }
+
+  @Input() turno:any = {};
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal, private scheduleAppointmentService: ScheduleAppointmentService) {
     // customize default values of modals used by this component tree
     config.backdrop = 'static';
     config.keyboard = false;
@@ -22,7 +28,12 @@ export class AppointmentComponent implements OnInit {
   }
 
   scheduleAppointment(){
-    console.log("turno pedido correctamente");
+    let body = 
+    {
+      "clinic_appointment_schedule_id" : this.turno.id,
+      "appointment_date": this.turno.appointment_date.substring(0,10)
+    }
+    this.scheduleAppointmentService.execute(this,body);
   }
 
 }
