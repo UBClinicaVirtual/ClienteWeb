7ff02e89230ecc1ad7abc9c814cec729b29ba66d
PedroRodriguez95 <Pedro.rodriguez6@comunidad.ub.edu.at>
2019-06-07 17:15:15 -0300

    Se agregan providers para el .spec de schedule-appointment.service

--- src/app/services/server-connection/requests/appointments/schedule-appointment.service.spec.ts
+++ src/app/services/server-connection/requests/appointments/schedule-appointment.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ScheduleAppointmentService } from './schedule-appointment.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('ScheduleAppointmentService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers : [ HttpClient, HttpHandler]
+  }));
 
   it('should be created', () => {
     const service: ScheduleAppointmentService = TestBed.get(ScheduleAppointmentService);
