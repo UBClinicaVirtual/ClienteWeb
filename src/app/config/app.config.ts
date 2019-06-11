efb0ce94b35d5f05b3827838e6a6bec7301b87ab
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2019-06-11 12:44:07 -0300

    Se separa el json role control a app.config.ts

---
+++ src/app/config/app.config.ts
@@ -0,0 +1,32 @@
+export const menuSidebar =
+    [
+        {   "titulo": 'Dashboard',
+            "userType":[1,2,3],
+            "url": '/dashboard'
+        },
+        {
+            "titulo":"Buscar Turno",
+            "userType":[1],
+            "url":"/search-turn"
+        },
+        {
+            "titulo":"Mis turnos",
+            "userType":[1,2],
+            "url":"/turnos"
+        },
+        {
+            "titulo":"Reporte Medico",
+            "userType":[2],
+            "url":"/hcp-report"
+        },
+        {
+            "titulo":"Buscar Doctores",
+            "userType":[3],
+            "url":"/search-hcps"
+        },
+        {
+            "titulo":"Mi Perfil",
+            "userType":[1,2,3],
+            "url":"/protected"
+        }
+    ]
--- src/app/config/usertype-config.service.ts
+++ src/app/config/usertype-config.service.ts
@@ -1,53 +1,20 @@
 import { Injectable } from '@angular/core';
 import { ServerConnectionService } from '../services/server-connection/server-connection.service';
 import { isInteger, toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
+import { menuSidebar } from './app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsertypeConfigService {
- constructor(private serviceConnectionService: ServerConnectionService){}
 
-  appconfig = {
-      "menuSidebar":
-      [
-          {   "titulo": 'Dashboard', 
-              "userType":[1,2,3],
-              "url": '/dashboard'
-          },
-          {
-              "titulo":"Buscar Turno",
-              "userType":[1],
-              "url":"/search-turn"
-          },
-          {
-              "titulo":"Mis turnos",
-              "userType":[1,2],
-              "url":"/turnos"
-          },
-          {
-              "titulo":"Reporte Medico",
-              "userType":[2],
-              "url":"/hcp-report"
-          },
-          {
-              "titulo":"Buscar Doctores",
-              "userType":[3],
-              "url":"/search-hcps"
-          },
-          {
-              "titulo":"Mi Perfil",
-              "userType":[1,2,3],
-              "url":"/protected"
-          }
-      ]
-  }
+ constructor(private serviceConnectionService: ServerConnectionService){}
 
 generateMenu(){
 
   let userType = Number(this.serviceConnectionService.userType);
 
-  return this.appconfig.menuSidebar.map(function(data,){
+  return menuSidebar.map(function(data,){
       if(data.userType.find(function(boton){
           return boton == userType;
       })){
