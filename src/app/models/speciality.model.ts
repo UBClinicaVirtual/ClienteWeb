2429d441944e9d3a771272e7d9505dab78605d5a
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-08 20:22:46 -0300

    creo el model speciality

---
+++ src/app/models/speciality.model.ts
@@ -0,0 +1,10 @@
+export class Especialidad {
+
+    constructor (
+        public id: string,
+        public name: string,
+        public created_at?: string,
+        public updated_at?: string
+    ) { }
+
+}
