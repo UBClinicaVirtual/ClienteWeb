ee70046fa008a7ae5a57acf7a048b2b77989c84a
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-08 20:23:47 -0300

    creo el ts specialities compoenent

---
+++ src/app/pages/specialities/specialities/specialities.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { Especialidad } from 'src/app/models/speciality.model';
+import { SpecialityService } from 'src/app/services/service.index';
+
+@Component({
+  selector: 'app-specialities',
+  templateUrl: './specialities.component.html',
+  styleUrls: ['./specialities.component.css']
+})
+export class SpecialitiesComponent implements OnInit {
+
+  specialities: Especialidad[] = [];
+
+  constructor(
+    public _specialityService: SpecialityService
+  ) { }
+
+  ngOnInit() {
+    this.cargarEspecialidades();
+  }
+
+  buscarEspecialidad(termino:string){
+    /*
+    if(termino.length <=0 ){
+      this.cargarHospitales();
+      return;
+    }
+    this._hospitalService.buscarHospital(termino)
+            .subscribe(hospitales => this.hospitales = hospitales );
+            */
+  }
+
+  cargarEspecialidades(){
+
+    this._specialityService.cargarEspecialidades()
+        .subscribe(specialities => this.specialities = specialities);
+        console.log(this.specialities);
+    /*
+    this._hospitalService.cargarHospitales()
+    .subscribe(hospitales => this.hospitales=hospitales);
+    console.log(this.hospitales);
+    */
+  }
+
+  guardarEspecialidad(speciality: Especialidad){
+    /*
+    this._hospitalService.actualizarHospital(hospital)
+    .subscribe();
+    */
+
+  }
+
+  crearEspecialidad(){
+    /*
+    swal({
+      title: 'Crear hospital',
+      text: 'Ingrese el nombre del hospital',
+      content: 'input',
+      icon: 'info',
+      buttons: true,
+      dangerMode: true,
+    }).then((valor: string) => {
+      if(!valor || valor.length ===0){
+        return;
+      }
+
+      this._hospitalService.crearHospital(valor)
+            .subscribe(()=>this.cargarHospitales());
+    })
+
+    */
+  }
+
+
+}
