a5973dc252a162fa3a08c68a74b646ba73c391dd
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-08 20:23:10 -0300

    agrego la ruta al componente speciality

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -9,6 +9,8 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 import { AppointmentSearchComponent } from '../components/appointment-search/appointment-search.component'
 import { LoginGuardGuard } from '../services/service.index';
 import { ProtectedComponent } from '../components/protected/protected.component';
+import { SpecialitiesComponent } from './specialities/specialities/specialities.component';
+
 
 
 
@@ -25,6 +27,7 @@ const pagesRoutes: Routes = [
             {path: 'search-turn', component:AppointmentSearchComponent, data: {titulo: 'Buscar Turnos'}},
             {path: 'account-settings', component:AccountSettingsComponent, data: {titulo: 'Ajustes de tema'}},
             {path: 'protected', component:ProtectedComponent, data: {titulo: 'Mi perfil'}},
+            {path: 'specialities', component:SpecialitiesComponent, data: {titulo: 'Especialidades'}},
             {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
     ]}
 
