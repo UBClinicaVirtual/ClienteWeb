a0785998cfd3dded9ec770de3e021d9c0b221381
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-09 01:57:01 -0300

    importo y proveo el hcpservice

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -8,7 +8,8 @@ import {
       UsuarioService,
       LoginGuardGuard,
       SpecialityService,
-      ClinicsService
+      ClinicsService,
+      HcpService
  } from "./service.index";
 
 
@@ -24,7 +25,8 @@ import {
     UsuarioService,
     LoginGuardGuard,
     SpecialityService,
-    ClinicsService
+    ClinicsService,
+    HcpService
   ]
 })
 export class ServiceModule { }
