790dfc7d35fcf7f22dff5230aff66b54b06565a1
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-09 00:38:08 -0300

    agrego los metodos cargarclinicas y buscarclinica

--- src/app/services/clinics/clinics.service.ts
+++ src/app/services/clinics/clinics.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ServerConfigService } from 'src/app/config/server-config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClinicsService {
 
-  constructor() { }
+  constructor(
+    public http: HttpClient,
+    private config: ServerConfigService
+  ) { }
+
+  cargarClinicas(){
+    let httpHeaders = new HttpHeaders({
+      'Authorization' : 'Bearer ' + localStorage.getItem('token')
+    });
+    let body = {
+      
+    }
+
+    let url = this.config.url() + '/clinic/search';
+
+    return this.http.post(url,body,{headers: httpHeaders});
+
+  }
+
+  buscarClinica(termino:string){
+    let httpHeaders = new HttpHeaders({
+      'Authorization' : 'Bearer ' + localStorage.getItem('token')
+    });
+    let body = {
+      "name": termino
+    }
+    let url = this.config.url() + '/clinic/search';
+    return this.http.post(url,body,{headers: httpHeaders});
+
+  }
 }
