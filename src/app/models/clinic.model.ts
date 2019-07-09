c8a1f4e323977230ceac35559ad516c2bf2dd6e3
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-09 00:38:37 -0300

    creo el clinic model

---
+++ src/app/models/clinic.model.ts
@@ -0,0 +1,15 @@
+export class Clinics {
+
+    constructor (
+        public id: string,
+        public created_at: string,
+        public updated_at: string,
+        public business_name: string,
+        public business_number: string,
+        public address: string,
+        public phone: string
+       
+    ) { }
+
+}
+
