91c0d27ce20f9f562afb7e53991c94952acd43b3
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-09 00:37:06 -0300

    agrego otro submenu al principal. el de clinicas

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -7,12 +7,10 @@ export class SidebarService {
 
   menu:any = [
     {
-      titulo: 'Principal',
+      titulo: 'Turnos',
       icono: 'mdi mdi-gauge',
       submenu: [
         {titulo: 'Dashboard', url: '/dashboard'},
-        // {titulo: 'Barra de progreso (prueba) ', url: '/progress'},
-        // {titulo: 'Graficas(prueba)', url: '/graficas1'},
         {titulo: 'Mis turnos', url: '/turnos'},
         {titulo: 'Buscar Turnos', url: '/search-turn'},
         
@@ -24,7 +22,15 @@ export class SidebarService {
       submenu: [
         {titulo: 'Ver/Modificar/Crear', url: '/specialities'},
       ]
+    },
+    {
+      titulo: 'Clinicas',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu: [
+        {titulo: 'Ver Clinicas', url: '/clinics'},
+      ]
     }
+
   ];
 
   constructor() { }
