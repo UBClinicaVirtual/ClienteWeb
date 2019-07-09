3aeb1d375e0320bed257552766728c60f3b1e465
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2019-07-09 00:38:22 -0300

    agrego los metodos cargar y buscar clinica

--- src/app/pages/clinics/clinics/clinics.component.ts
+++ src/app/pages/clinics/clinics/clinics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Clinics } from 'src/app/models/clinic.model';
+import { ClinicsService } from 'src/app/services/service.index';
 
 @Component({
   selector: 'app-clinics',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClinicsComponent implements OnInit {
 
-  constructor() { }
+  cargando: boolean = true;
+  clinics: Clinics [] = [];
+
+  constructor(
+    public _clinicService: ClinicsService
+  ) { }
 
   ngOnInit() {
+    this.cargarClinicas();
+  }
+
+  cargarClinicas(){
+    this.cargando = true;
+
+    this._clinicService.cargarClinicas()
+        .subscribe((resp:any)=> {
+          this.clinics = resp.clinics;
+          this.cargando = false;
+        })
+  }
+
+  buscarClinica(termino:string){
+    if(termino.length <= 0){
+      this.cargarClinicas();
+      return;
+    }
+    if(termino.length <=2){
+      return;
+    }
+    this.cargando=true;
+    this._clinicService.buscarClinica(termino)
+          .subscribe((resp:any)=> {
+            this.clinics = resp.clinics;
+            this.cargando = false;
+          });
   }
 
 }
